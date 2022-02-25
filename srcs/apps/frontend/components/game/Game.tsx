fb88b3447463009ff9316cf0914dbbfcfca5e01d
Abdelmouhaymine Salah <abdelmouhayminegx@gmail.com>
2022-02-25 19:33:16 +0100

    all

--- srcs/apps/frontend/components/game/Game.tsx
+++ srcs/apps/frontend/components/game/Game.tsx
@@ -1,18 +1,109 @@
 import React, { useRef, useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import logo from "./logo.svg";
+import { useSocket } from "../../providers/SocketProvider";
 import Swal from "sweetalert2";
 import { useDispatch } from "react-redux";
-import { useLocation, useNavigate } from "react-router";
-import { cancelJoin } from "../../app/features/game";
+import { useNavigate } from "react-router";
+import { cancelJoin, joinGame } from "../../app/features/game";
 import { useAppSelector } from "../../app/hooks";
-import { useSocket } from "../../providers/SocketProvider";
-import WaitingGame from "./components/WaitingGame";
 
-export enum UserType {
-  Player,
-  Spectator,
+const WIDTH = 1100;
+const HEIGHT = 600;
+const PADDLE_HEIGHT = HEIGHT / 6;
+const PADDLE_WIDTH = 20;
+const L_PADDLE_X = 0;
+const R_PADDLE_X = WIDTH - PADDLE_WIDTH;
+const MIDDLE_PADDLE_INIT_Y = HEIGHT - PADDLE_HEIGHT - PADDLE_WIDTH / 2;
+const PADDLE_INIT_Y = HEIGHT / 2 - PADDLE_HEIGHT / 2;
+const M_PADDLE_X = WIDTH / 2 - PADDLE_WIDTH / 2;
+
+class Circle {
+  _ctx: CanvasRenderingContext2D;
+  _x: number;
+  _y: number;
+  _r: number;
+  _color: string;
+
+  constructor(
+    ctx: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    r: number,
+    color: string
+  ) {
+    this._ctx = ctx;
+    this._x = x;
+    this._y = y;
+    this._r = r;
+    this._color = color;
+  }
+  draw() {
+    this._ctx.beginPath();
+    this._ctx.fillStyle = this._color;
+    this._ctx.arc(this._x, this._y, this._r, 0, Math.PI * 2);
+    this._ctx.fill();
+    this._ctx.closePath();
+  }
 }
 
-// game state
+class Ball extends Circle {}
+
+class Rect {
+  _ctx: CanvasRenderingContext2D;
+  _x: number;
+  _y: number;
+  _width: number;
+  _height: number;
+  _color: string;
+
+  constructor(
+    ctx: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    color: string
+  ) {
+    this._ctx = ctx;
+    this._x = x;
+    this._y = y;
+    this._width = width;
+    this._height = height;
+    this._color = color;
+  }
+  draw() {
+    this._ctx.beginPath();
+    this._ctx.fillStyle = this._color;
+    this._ctx.rect(this._x, this._y, this._width, this._height);
+    this._ctx.fill();
+    this._ctx.closePath();
+  }
+}
+
+class Paddle extends Rect {
+  drawWithCircle() {
+    const upCircle = new Circle(
+      this._ctx,
+      this._x + PADDLE_WIDTH / 2,
+      this._y,
+      PADDLE_WIDTH / 2,
+      this._color
+    );
+    const downCircle = new Circle(
+      this._ctx,
+      this._x + PADDLE_WIDTH / 2,
+      this._y + PADDLE_HEIGHT,
+      PADDLE_WIDTH / 2,
+      this._color
+    );
+
+    this.draw();
+    upCircle.draw();
+    downCircle.draw();
+  }
+}
+
 const enum GameState {
   WAITING,
   STARTING,
@@ -21,16 +112,9 @@ const enum GameState {
   OVER,
 }
 
-interface FrameShape {
-  ball: {
-    x: number;
-    y: number;
-  };
-  paddles: {
-    ly: number;
-    ry: number;
-    my?: number;
-  };
+interface IFrame {
+  ball: { x: number; y: number };
+  paddles: { ly: number; ry: number; my?: number };
   score: {
     p1: number;
     username1: string;
@@ -40,36 +124,20 @@ interface FrameShape {
     image2: string;
   };
   state: GameState;
-  startingSecond?: number;
   hasMiddlePaddle: boolean;
   hasWon: boolean;
+  startingSecond?: number;
   message?: string;
 }
 
-const Game = ({ userType }: { userType: UserType }) => {
-
-  // constants
-  const socket = useSocket();
-  const Canvasref = useRef();
-  const WIDTH = 1100;
-  const HEIGHT = 600;
-  const PADDLE_HEIGHT = HEIGHT / 6;
-  const PADDLE_WIDTH = 20;
-  const BALL_RADIUS = 9;
-  const L_PADDLE_X = 0;
-  const R_PADDLE_X = WIDTH - PADDLE_WIDTH;
-  const M_PADDLE_X = WIDTH / 2 - PADDLE_WIDTH / 2;
-  const PADDLE_INIT_Y = HEIGHT / 2 - PADDLE_HEIGHT / 2;
-  const MIDDLE_PADDLE_INIT_Y = HEIGHT - PADDLE_HEIGHT - PADDLE_WIDTH / 2;
-
+function Game() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const socket = useSocket();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const joined = useAppSelector((state) => state.game.joined);
 
-  function delay(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-  // initial state
-  const initialState: FrameShape = {
+  const initialState = {
     ball: {
       x: WIDTH / 2,
       y: HEIGHT / 2,
@@ -81,45 +149,69 @@ const Game = ({ userType }: { userType: UserType }) => {
     },
     score: {
       p1: 0,
-      username1: '',
-      image1: '',
+      username1: "",
+      image1: "",
       p2: 0,
-      username2: '',
-      image2: '',
+      username2: "",
+      image2: "",
     },
     state: GameState.WAITING,
     hasMiddlePaddle: false,
     hasWon: false,
   };
-  // state of the frame
-  const [frame, setframe] = useState(initialState);
-  // if the user is a spectator
-  const { state } = useLocation();
-  useEffect(() => {
-    if (userType === UserType.Spectator && !state) {
-      navigate('/liveGames');
-    }
-  });
-  if (userType === UserType.Spectator && frame.state === GameState.WAITING) {
-    socket.emit("spectator", state);
-    socket.off("spectator");
-  }
+  const [frame, setFrame] = useState<IFrame>(initialState);
+  const gameListBtnRef: React.RefObject<HTMLDivElement> = React.createRef();
+  const controlRef: React.RefObject<HTMLDivElement> = React.createRef();
+  const imgRef: React.RefObject<HTMLDivElement> = React.createRef();
+  const quitRef: React.RefObject<HTMLDivElement> = React.createRef();
 
-  useEffect(() => {
-    // set up the socket
-    socket.on("state", (newFrame) => {
-      setframe(newFrame);
+  const startGame = function (gameType: string) {
+    socket.emit("join_queue_match", gameType);
+    dispatch(joinGame());
+  };
+
+  useEffect(
+    function () {
+      socket.on("state", function (newFrame: any) {
+        setFrame(newFrame);
+      });
+      return () => {
+        socket.off("state");
+      };
+    },
+    [socket]
+  );
+
+  const quit = async function () {
+    const result = await Swal.fire({
+      title: "Are you sure?",
+      text: "You will lose your score!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD5454",
+      cancelButtonColor: "#3085d6",
+      confirmButtonText: "Yes, leave!",
     });
-    return () => {
-      socket.off("state");
-    };
-  }, [socket]);
+    if (result.isConfirmed) {
+      socket.emit("leave_game");
+      dispatch(cancelJoin());
+      //setFrame({...initialState});
+      navigate("/game");
+    }
+  };
 
-  const auth = useAppSelector(state => state.user);
+  const gameOver = function (ctx: CanvasRenderingContext2D, text: string) {
+    ctx.beginPath();
+    ctx.font = "30px game-font";
+    ctx.fillStyle = "black";
 
-  useEffect(() => {
-    // move the paddle down
-    if (userType === UserType.Spectator || frame.state === GameState.STARTING) return;
+    const metrics = ctx.measureText(text);
+
+    ctx.fillText(text, WIDTH / 2 - metrics.width / 2, 50);
+    ctx.closePath();
+  };
+
+  useEffect(function () {
     document.addEventListener("keydown", (e) => {
       if (e.code === "ArrowUp") {
         socket.emit("up_paddle", "down");
@@ -127,7 +219,6 @@ const Game = ({ userType }: { userType: UserType }) => {
         socket.emit("down_paddle", "down");
       }
     });
-    // move the paddle up
     document.addEventListener("keyup", (e) => {
       if (e.code === "ArrowUp") {
         socket.emit("up_paddle", "up");
@@ -135,216 +226,161 @@ const Game = ({ userType }: { userType: UserType }) => {
         socket.emit("down_paddle", "up");
       }
     });
-    // pause the game
-    document.addEventListener("keypress", (e) => {
-      if (e.code === "Space") {
-        socket.emit("pause_game");
-      }
-    });
   }, []);
 
-  useEffect(() => {
-    if (frame.state === GameState.WAITING && userType === UserType.Player) return;
-    let c: any = Canvasref.current;
-    let ctx = c.getContext("2d");
-
-    function clear_init(x: number, y: number, w: number, h: number) {
-      ctx.clearRect(x, y, w, h);
-      ctx.beginPath();
-      ctx.fillStyle = "#1F2937";
-      ctx.fillRect(x, y, w, h);
-      draw_separator();
-    }
-
-    function draw_ball(color: string, x: number, y: number, radius: number) {
-      ctx.beginPath();
-      ctx.arc(x, y, radius, 0, 2 * Math.PI);
-      ctx.fillStyle = color;
-      ctx.fill();
-    }
-
-    function draw_paddle(color: string, x: number, y: number) {
-      ctx.fillStyle = color;
-      ctx.fillRect(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
-      ctx.beginPath();
-      draw_ball(color, x + PADDLE_WIDTH / 2, y, PADDLE_WIDTH / 2);
-      draw_ball(
-        color,
-        x + PADDLE_WIDTH / 2,
-        y + PADDLE_HEIGHT,
-        PADDLE_WIDTH / 2
-      );
-    }
-
-    function draw_text(text: string, color: string, font: string, x: number, y: number) {
-      ctx.fillStyle = color;
-      ctx.font = font;
-      ctx.fillText(text, x, y);
-    }
-
-    function draw_separator() {
-      ctx.strokeStyle = "#FFFFFF";
-      ctx.setLineDash([10, 15]);
-      ctx.beginPath();
-      ctx.moveTo(WIDTH / 2, 0);
-      ctx.lineTo(WIDTH / 2, HEIGHT);
-      ctx.stroke();
-    }
-
-    async function draw() {
-      clear_init(0, 0, WIDTH, HEIGHT);
-      draw_text(
-        frame.score.p1.toString(),
-        "#FFFFFF",
-        "32px Arcade Normal",
-        3 * (WIDTH / 8),
-        HEIGHT / 12
-      );
-      draw_text(
-        frame.score.p2.toString(),
-        "#FFFFFF",
-        "32px Arcade Normal",
-        5 * (WIDTH / 8) - 5,
-        HEIGHT / 12
-      );
-      if (frame.state !== GameState.STARTING)
-        draw_ball("#FFEE00", frame.ball.x, frame.ball.y, BALL_RADIUS);
-
-      draw_paddle("#00D897", L_PADDLE_X, frame.paddles.ly);
-      if (frame.hasMiddlePaddle) {
-        draw_paddle("#D6D6D6", M_PADDLE_X, frame.paddles.my);
+  useEffect(
+    function () {
+      if (canvasRef == null) {
+        return;
       }
+      const canvas = canvasRef.current;
+      const ctx = canvas != null ? canvas.getContext("2d") : null;
 
-      draw_paddle("#00D897", R_PADDLE_X, frame.paddles.ry);
+      if (
+        ctx != null &&
+        frame.state != GameState.WAITING &&
+        frame.state != GameState.OVER
+      ) {
+        if (canvas) canvas.style.display = "block";
 
-      if (frame.state === GameState.STARTING) {
-        draw_text(
-          frame.startingSecond?.toString(),
-          "#FFFFFF",
-          "70px Arcade Normal",
-          WIDTH / 2 - 25,
-          HEIGHT / 2 + 25
+        if (quitRef != null && frame.state == GameState.PLAYING)
+          if (quitRef.current != null) quitRef.current.style.display = "flex";
+
+        if (controlRef != null)
+          if (controlRef.current != null)
+            controlRef.current.style.display = "none";
+
+        const background = new Rect(
+          ctx,
+          0,
+          0,
+          ctx.canvas.width,
+          ctx.canvas.height,
+          "rgb(253 230 138)"
         );
-      }
-      if (frame.message) {
-        if (frame.message === "Pause") {
-        draw_text(
-          frame.message.toString(),
-          "#FFFFFF",
-          "70px Arcade Normal",
-          WIDTH / 2 - 150,
-          HEIGHT / 2 + 60
+
+        background.draw();
+
+        const paddleLeft = new Paddle(
+          ctx,
+          L_PADDLE_X,
+          frame.paddles.ly,
+          PADDLE_WIDTH,
+          PADDLE_HEIGHT,
+          "rgb(25 109 180)"
         );
-        } else {
-          draw_text(
-            frame.message.toString(),
-            "#FC7A00",
-            "30px Arcade Normal",
-            WIDTH / 2 - 150,
-            HEIGHT / 2 + 180
+        const paddleRight = new Paddle(
+          ctx,
+          R_PADDLE_X,
+          frame.paddles.ry,
+          PADDLE_WIDTH,
+          PADDLE_HEIGHT,
+          "black"
+        );
+        const ball = new Ball(ctx, frame.ball.x, frame.ball.y, 10, "black");
+
+        paddleLeft.drawWithCircle();
+        paddleRight.drawWithCircle();
+        ball.draw();
+
+        if (frame.hasMiddlePaddle == true) {
+          const paddleMiddle = new Paddle(
+            ctx,
+            M_PADDLE_X,
+            frame.paddles.my,
+            PADDLE_WIDTH,
+            PADDLE_HEIGHT,
+            "rgb(25 109 180)"
           );
-        }
-      }
 
-      if (frame.state === GameState.OVER) {
-        // tell everyone that the game is over
-        draw_text(
-          "Game Over",
-          "#FFFFFF",
-          "32px Arcade Normal",
-          WIDTH / 2 - 125,
-          HEIGHT / 3
+          paddleMiddle.drawWithCircle();
+        }
+      } else if (frame.state == GameState.WAITING) {
+        if (canvas) canvas.style.display = "none";
+        if (quitRef != null)
+          if (quitRef.current != null) quitRef.current.style.display = "none";
+
+        if (controlRef != null)
+          if (controlRef.current != null)
+            controlRef.current.style.display = "block";
+        if (gameListBtnRef != null)
+          if (gameListBtnRef.current != null)
+            gameListBtnRef.current.style.display = "block";
+      } else if (ctx != null && frame.state == GameState.OVER) {
+        const background = new Rect(
+          ctx,
+          0,
+          0,
+          ctx.canvas.width,
+          ctx.canvas.height,
+          "rgb(253 230 138)"
         );
-        if (userType === UserType.Player) {
-          // tell the player if they won or not
-          if (frame.hasWon)
-            draw_text(
-              "You Won",
-              "#00FF15",
-              "45px Arcade Normal",
-              WIDTH / 2 - 150,
-              2 * (HEIGHT / 3)
-            );
-          else
-            draw_text(
-              "You Lost",
-              "#FF0000",
-              "45px Arcade Normal",
-              WIDTH / 2 - 150,
-              2 * (HEIGHT / 3)
-            );
-        }
-        else {
-          // tell the spectator who won
-          draw_text(
-            (frame.score.p1 === 10 ? frame.score.username1 : frame.score.username2) + " Won",
-            "#00FF15",
-            "40px Arcade Normal",
-            WIDTH / 2 - 150,
-            2 * (HEIGHT / 3)
-          );
-        }
 
+        if (quitRef != null)
+          if (quitRef.current != null) quitRef.current.style.display = "none";
+        background.draw();
+        if (frame.hasWon == true) gameOver(ctx, "YOU WON");
+        else gameOver(ctx, "YOU LOST");
+        setTimeout(() => {
+          dispatch(cancelJoin());
+          setFrame({ ...initialState });
+          navigate("/game");
+        }, 3000);
       }
-    }
-    draw();
+    },
+    [frame]
+  );
 
-  }, [frame, M_PADDLE_X, PADDLE_HEIGHT, R_PADDLE_X, userType]);
-  if (frame.state === GameState.OVER) {
-    delay(5000).then(() => {
-      dispatch(cancelJoin());
-      setframe({ ...initialState });
-      navigate('/game');
-    });
-  }
-
-  const leaveGame = () => {
-    Swal.fire({
-      title: 'Are you sure?',
-      text: "You will lose your score!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#DD5454',
-      cancelButtonColor: '#3085d6',
-      confirmButtonText: 'Yes, leave!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        socket.emit('leave_game');
-        dispatch(cancelJoin());
-        setframe({ ...initialState });
-        navigate('/game');
-      }
-    })
-  };
-
-  return frame.state !== GameState.WAITING || userType === UserType.Spectator ?
-    (
-      <div className="game-bg flex flex-col justify-center items-center h-full w-full " >
-        <div className="with_1100 flex flex-row items-center justify-between px-3 mb-3">
-          <div className="flex flex-row items-center ">
-            <img src={frame.score.image1} className="select-none pointer-events-none mr-3 object-cover object-center w-16 md:w-20 h-16 md:h-20 flex-none rounded-full border-2 border-gray-500 shadow-slate-500 shadow-xl" />
-            <h2 className="text-xl font-semibold tracking-wider select-none game-username text-GameFontColor">{frame.score.username1}</h2>
+  return (
+    <div>
+      <div className="container board">
+        {frame.state != GameState.WAITING ? (
+          <div className="container board-wrap">
+            <div className="board-info">
+              <span>{frame.score.username1}</span>
+              <span>{frame.score.p1}</span>
+            </div>
+            <div className="board-info">
+              <span>{frame.score.username2}</span>
+              <span>{frame.score.p2}</span>
+            </div>
           </div>
-          <h1 className="h1 select-none text-GameFontColor"> Trans-Pong </h1>
-          <div className="flex flex-row items-center">
-            <h2 className="text-xl font-semibold tracking-wider select-none game-username text-GameFontColor">{frame.score.username2}</h2>
-            <img src={frame.score.image2} className="select-none pointer-events-none ml-3 object-cover object-center w-16 md:w-20 h-16 md:h-20 flex-none rounded-full border-2 border-gray-500 shadow-slate-500 shadow-xl" />
-          </div>
-        </div>
-        <canvas ref={Canvasref} className="mb-5" width={WIDTH} height={HEIGHT} />
-        {
-          (frame.state === GameState.PLAYING || frame.state === GameState.PAUSED) &&
-          userType === UserType.Player &&
-          <button
-            onClick={leaveGame}
-            className="button select-none flex px-6 pt-3 pb-2 bg-red-400 text-white rounded-full shadow-lg text-sm"
-          >
-            Leave
-          </button>}
+        ) : (
+          ""
+        )}
       </div>
-    )
-    :
-    <WaitingGame />;
-};
-
+      <canvas
+        className="my-canvas"
+        width={WIDTH}
+        height={HEIGHT}
+        ref={canvasRef}
+      />
+      <div ref={controlRef} className="game-control">
+        <div className="game-sub-control">
+          {!joined ? (
+            <div ref={gameListBtnRef} className="game-list-btn">
+              <button className="game-btn" onClick={() => startGame("dual")}>
+                DUAL PONG
+              </button>
+              <br />
+              <button className="game-btn" onClick={() => startGame("triple")}>
+                TRIPLE PONG
+              </button>
+            </div>
+          ) : (
+            <div ref={imgRef} className="wait-gif">
+              <b>waiting for opponent...</b>
+              <img src="./waiting.gif" />
+            </div>
+          )}
+        </div>
+      </div>
+      <div ref={quitRef} className="container quit-container-btn">
+        <button className="quit-btn" onClick={quit}>
+          quit
+        </button>
+      </div>
+    </div>
+  );
+}
 export default Game;
--- srcs/apps/frontend/tailwind.config.js
+++ srcs/apps/frontend/tailwind.config.js
@@ -1,29 +1,30 @@
-module.exports = {  
-	content: [    "./pages/**/*.{js,ts,jsx,tsx}",    "./components/**/*.{js,ts,jsx,tsx}",  ],  
-	theme: {
-		extend: {
-		  colors: {
-			BGF: '#654ea3',
-			BGT: '#eaafc8',
-			LBLUE: '#ebf4fa',
-			VsColor: '#005DAB',
-			GameFontColor: '#354964'
-		  },
-		  spacing: {
-			'100': '26rem',
-			'110': '30rem',
-			'120': '35rem',
-			'130': '40rem',
-			'140': '45rem',
-			'200': '60rem',
-		  },
-		  screens: {
-			'3xl': '1620px',
-			'lmd': '840px'
-		  },
-		},
-	  }, 
-	  plugins: [
-		require("tailwind-scrollbar-hide"),
-	  ]
-}
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        BGF: "#654ea3",
+        BGT: "#eaafc8",
+        LBLUE: "#FFFDD0",
+        VsColor: "#005DAB",
+        GameFontColor: "#354964",
+      },
+      spacing: {
+        100: "26rem",
+        110: "30rem",
+        120: "35rem",
+        130: "40rem",
+        140: "45rem",
+        200: "60rem",
+      },
+      screens: {
+        "3xl": "1620px",
+        lmd: "840px",
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
